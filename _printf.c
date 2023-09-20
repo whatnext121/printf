3cb1f036699e2d94f3a24e6807a7e1f8cff76036
whatnext121 <adetounolamidejoseph@gmail.com>
2023-09-20 14:18:50 -0700

    formated string

--- _printf.c
+++ _printf.c
@@ -1,25 +1,23 @@
 #include "main.h"
-/**
- * _printf - printf string
- * @format: inputed string
- * @..: variable number of argument
- * print_buffer - Prints the contents of the buffer
- * @buffer: Array of chars
- * @buff_ind: The length of char.
- * Return: char variable
- */
+
 void print_buffer(char buffer[], int *buff_ind);
+
+/**
+ * _printf - Printf function
+ * @format: format string.
+ * Return: printed chars.
+ */
 int _printf(const char *format, ...)
 {
-	int i, printed = 0, count = 0;
+	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
-	va_list My_list;
+	va_list list;
 	char buffer[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(My_list, format);
+	va_start(list, format);
 
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
@@ -28,26 +26,37 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			count++;
+			
+			printed_chars++;
 		}
 		else
 		{
 			print_buffer(buffer, &buff_ind);
 			flags = get_flags(format, &i);
-			width = get_width(format, &i, My_list);
-			precision = get_precision(format, &i, My_list);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
-			i++;
-			printed = handle_print(format, &i, My_list, buffer, flags, width, precision, size);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
-			count += printed;
+			printed_chars += printed;
 		}
-	print_buffer(buffer, &buff_ind);
-	va_end(My_list);
-	return (count);
 	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
+
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
 void print_buffer(char buffer[], int *buff_ind)
 {
 	if (*buff_ind > 0)
@@ -55,3 +64,4 @@ void print_buffer(char buffer[], int *buff_ind)
 
 	*buff_ind = 0;
 }
+
